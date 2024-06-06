3372cdf5d21dbba13730b2ae1682c36c457369fa
Andy Garcia <barcag07@icloud.com>
2024-06-06 18:09:51 -0500

    Allow for margin or padding for buttons
    DOM elements were modified to account for the margin and padding that
    the zero button takes up to allow for 4 buttons per row.

--- js/javascript.js
+++ js/javascript.js
@@ -9,20 +9,27 @@ const buttonsDiv = document.querySelector(".buttons");
 
 
 const buttonItems = buttonsDiv.querySelectorAll("button");
-//Because the querySelectorAll is used we have to change the variable to an array and use the spread operator
-let buttonHW = calcWidth / 4;
+let buttonMargin = 5;
+let buttonPadding = 0;
+let totalButtonHW = calcWidth / 4;
+let buttonHW = totalButtonHW - (buttonMargin * 2) - (buttonPadding * 2);
 console.log(buttonHW);
 buttonItems.forEach((button) => {
      button.style.height = buttonHW + "px";
      button.style.width = buttonHW + "px";
      button.style.borderRadius = 100 + "%";
      button.style.border = "none";
+     button.style.margin = buttonMargin + "px";
+     button.style.padding = buttonPadding + "px";
 });
 
 
-const equalButton = buttonsDiv.querySelector(".zero");
-let equalButtonCSS = getComputedStyle(equalButton);
-let equalButtonWidth = parseFloat(equalButtonCSS.width);
-equalButton.style.width = (equalButtonWidth * 2) + "px";
-equalButton.style.borderRadius = 100 + "px";
+const zeroButton = buttonsDiv.querySelector(".zero");
+// let zeroButtonCSS = getComputedStyle(zeroButton);
+// let zeroButtonWidth = parseFloat(zeroButtonCSS.width);
+let zeroButtonWidth = buttonHW * 2 + buttonMargin * 2 + buttonPadding * 2;
+zeroButton.style.width = (zeroButtonWidth) + "px";
+zeroButton.style.borderRadius = 100 + "px";
+zeroButton.style.margin = buttonMargin;
+zeroButton.style.padding = buttonPadding;
 
