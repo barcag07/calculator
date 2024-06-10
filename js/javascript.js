0a0f9de20806249bae9bdf57ce5e4d5801f03c04
Andy Garcia <barcag07@icloud.com>
2024-06-10 00:42:45 -0500

    Fix operation and equal buttons when clicked
    When the operation buttons are clicked, they will get brighter. They
    will stay bright until the equal button is clicked and all the buttons
    return to original brightness.
    
    The addition button also works when selecting two numbers and pressing
    the equal button.

--- js/javascript.js
+++ js/javascript.js
@@ -24,7 +24,7 @@ buttonItems.forEach((button) => {
 });
 
 
-const zeroButton = buttonsDiv.querySelector(".zero");
+let zeroButton = buttonsDiv.querySelector(".zero");
 // let zeroButtonCSS = getComputedStyle(zeroButton);
 // let zeroButtonWidth = parseFloat(zeroButtonCSS.width);
 let zeroButtonWidth = buttonHW * 2 + buttonMargin * 2 + buttonPadding * 2;
@@ -33,16 +33,55 @@ zeroButton.style.borderRadius = 100 + "px";
 zeroButton.style.margin = buttonMargin;
 zeroButton.style.padding = buttonPadding;
 
+
+let outputContainer = calculator.querySelector(".output-container");
+let outputText = outputContainer.querySelector(".output-text")
+// outputText.textContent = "YOOOOOO";
+
+
+let operation;
+
+let operationButtons = buttonsDiv.querySelectorAll(".operator");
+operationButtons.forEach((button) => {
+     button.addEventListener("click", () => {
+          button.style.filter = `brightness(${125}%)`
+          operation = button.getAttribute("data-value");
+          console.log(operation);
+     });
+});
+
+
+
 let firstNum;
+let secondNum
 
 
 let numberButtons = buttonsDiv.querySelectorAll(".number");
 numberButtons.forEach((button) => {
      button.addEventListener("click", () => {
+          //When using .getAttribute it changes value to a string so don't forget to change the value to a number using Number(variable)
           let buttonValue = button.getAttribute("data-value");
+          buttonValue = Number(buttonValue);
+
           //if operation doesn't have a value then firstNum is first click else click is secondNum
-          firstNum = buttonValue;
-          console.log(firstNum);
+          if (!firstNum) {
+               firstNum = buttonValue;               
+               outputText.textContent = firstNum;
+
+          }
+          else if(firstNum && operation) {
+               secondNum = button.getAttribute("data-value");
+               secondNum = Number(secondNum);
+          }
+          else {}
+          // firstNum = buttonValue;
+
+          // //Check to see if there is a value for firstNum and operation
+          // if (operation && firstNum) {
+               
+          // }
+
+          console.log(`operation: ${operation} \nfirstNum: ${firstNum}\nsecondNum: ${secondNum}`);
      });
      button.addEventListener("mousedown", () => {
           button.style.filter = `brightness(${150}%)`;
@@ -52,19 +91,29 @@ numberButtons.forEach((button) => {
      })
 });
 
-let operation;
 
-let operationButtons = buttonsDiv.querySelectorAll(".operator");
-operationButtons.forEach((button) => {
-     button.addEventListener("click", () => {
-          operation = button.getAttribute("data-value");
-          console.log(operation);
-     })
+
+let equalButton = buttonsDiv.querySelector(".equals");
+equalButton.addEventListener("click", () => {
+     equal(firstNum, secondNum, operation);
+     equalButton.addEventListener("mousedown", () => {
+          equalButton.style.filter = `brightness(${125}%)`;
+     });
+     equalButton.addEventListener("mouseup", () => {
+          equalButton.style.filter = `brightness(${100}%)`;
+     });
+     operationButtons.forEach((button) => {
+          button.style.filter = `brightness(${100}%)`;
+     });
 });
+
+
 
 //Add event listener for decimal that when it is pressed, it gets appended to the the output text
 
 //OPERATIONS
+
+
 
 function clear() {
 
@@ -80,9 +129,10 @@ function back() {
 //Create a variable for firstNum, secondNum, and operation
 // takes 2 parameter, firstNum secondNum(maybe an event)
 // first a number is clicked and added to variable firstNum that holds 2st number
-//  Then the function operation is called on the click of that operation symbol. Inside the function it creates a variable maybe addTemp that on a click of another number, it gets assigned to that variable, then the two numbers get added and then the answer is given the value of firstNum and returned to the oputput text awaiting the next operation 
-function add() {
-
+//  Then the function operation is called on the click of that operation symbol. Inside the function it creates a variable maybe addTemp that on a click of another number, it gets assigned to that variable, then the two numbers get added and then the answer is given the value of firstNum and returned to the output text awaiting the next operation 
+function add(firstNum, secondNum) {
+     console.log("Addition Answer:",firstNum + secondNum);
+     return firstNum + secondNum;
 }
 
 function subtract() {
@@ -97,6 +147,8 @@ function multiply() {
 
 }
 
-function equal() {
-     
+function equal(firstNum, secondNum, operation) {
+     switch(operation) {
+          case "+": add(firstNum, secondNum);
+     }
 }
