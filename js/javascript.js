a147c71f9b308c0837cd6f4ea136099dddcd0ea6
Andy Garcia <barcag07@icloud.com>
2024-06-11 20:58:09 -0500

    Start project from scratch
    Most of the code is commented out to try a new approach. Some code was
    left for the basic operation and structure of the calculator.

--- js/javascript.js
+++ js/javascript.js
@@ -34,173 +34,202 @@ zeroButton.style.margin = buttonMargin;
 zeroButton.style.padding = buttonPadding;
 
 
-let topButtons = buttonsDiv.querySelector(".top");
+// let topButtons = buttonsDiv.querySelector(".top");
 
-let clearButton = buttonsDiv.querySelector(".clear");
-clearButton.addEventListener("click", () => {
-     clear();
-});
+// let clearButton = buttonsDiv.querySelector(".clear");
+// clearButton.addEventListener("click", () => {
+//      clear();
+// });
 
-let signChangeButton = buttonsDiv.querySelector(".positive-negative");
-signChangeButton.addEventListener("click", () => {
-     if(secondNum) {
-          secondNum = signChange(secondNum);
-          outputText.textContent = secondNum;
-     }
-     else if(firstNum) {
-          firstNum = signChange(firstNum);
-          outputText.textContent = firstNum;
-     }
-     else{};
+// let signChangeButton = buttonsDiv.querySelector(".positive-negative");
+// signChangeButton.addEventListener("click", () => {
+//      if (outputText.textContent !== "0") {
+//           if (operation && secondNum !== null) {
+//                secondNum = signChange(secondNum);
+//                outputText.textContent = secondNum;
+//           } else if (firstNum !== null) {
+//                firstNum = signChange(firstNum);
+//                outputText.textContent = firstNum;
+//           }
+//      }
+// });
+
+
+// let outputContainer = calculator.querySelector(".output-container");
+// let outputText = outputContainer.querySelector(".output-text")
+// // outputText.textContent = "YOOOOOO";
+
+
+// let operation;
+
+// let operationButtons = buttonsDiv.querySelectorAll(".operator");
+// operationButtons.forEach((button) => {
+//      button.addEventListener("click", () => {
+//           button.style.filter = `brightness(${125}%)`
+//           operation = button.getAttribute("data-value");
+//           console.log(operation);
+//      });
+// });
+
+// let decimalButton = buttonsDiv.querySelector(".decimal-point");
+// let decimalClicked = false;
+
+// decimalButton.addEventListener("click", () => {
+//      decimalClicked = true;
+// });
+
+
+
+
+// let firstNum;
+// let secondNum
+
+
+// let numberButtons = buttonsDiv.querySelectorAll(".number");
+// numberButtons.forEach((button) => {
+//      button.addEventListener("click", () => {
+//           //When using .getAttribute it changes value to a string so don't forget to change the value to a number using Number(variable)
+//           let buttonValue = button.getAttribute("data-value");
+//           appendToDisplay(buttonValue);
+
+//           // if (decimalClicked) {
+//           //      if (!firstNum) {
+//           //           firstNum += buttonValue;
+//           //           firstNum = buttonValue;
+
+//           //           outputText.textContent = firstNum;
      
-     
-});
+//           //      }
+//           //      else if(firstNum && operation) {
+//           //           secondNum = button.getAttribute("data-value");
+//           //           secondNum = Number(secondNum);
+//           //           outputText.textContent = secondNum;
+//           //      }
+//           //      else {}
+//           // }
+//           buttonValue = Number(buttonValue);
 
 
-let outputContainer = calculator.querySelector(".output-container");
-let outputText = outputContainer.querySelector(".output-text")
-// outputText.textContent = "YOOOOOO";
+//           // if()
 
+//           //if operation doesn't have a value then firstNum is first click else click is secondNum
 
-let operation;
+//           // if (!firstNum) {
+//           //      firstNum = buttonValue;               
+//           //      outputText.textContent = firstNum;
 
-let operationButtons = buttonsDiv.querySelectorAll(".operator");
-operationButtons.forEach((button) => {
-     button.addEventListener("click", () => {
-          button.style.filter = `brightness(${125}%)`
-          operation = button.getAttribute("data-value");
-          console.log(operation);
-     });
-});
-
-
-
-let firstNum;
-let secondNum
-
-
-let numberButtons = buttonsDiv.querySelectorAll(".number");
-numberButtons.forEach((button) => {
-     button.addEventListener("click", () => {
-          //When using .getAttribute it changes value to a string so don't forget to change the value to a number using Number(variable)
-          let buttonValue = button.getAttribute("data-value");
-          buttonValue = Number(buttonValue);
-
-
-          // if()
-
-          //if operation doesn't have a value then firstNum is first click else click is secondNum
-          if (!firstNum) {
-               firstNum = buttonValue;               
-               outputText.textContent = firstNum;
-
-          }
-          else if(firstNum && operation) {
-               secondNum = button.getAttribute("data-value");
-               secondNum = Number(secondNum);
-               outputText.textContent = secondNum;
-          }
-          else {}
+//           // }
+//           // else if(firstNum && operation) {
+//           //      secondNum = button.getAttribute("data-value");
+//           //      secondNum = Number(secondNum);
+//           //      outputText.textContent = secondNum;
+//           // }
+//           // else {}
 
           
-          // firstNum = buttonValue;
+//           // firstNum = buttonValue;
 
-          // //Check to see if there is a value for firstNum and operation
-          // if (operation && firstNum) {
+//           // //Check to see if there is a value for firstNum and operation
+//           // if (operation && firstNum) {
                
-          // }
+//           // }
 
-          console.log(`operation: ${operation} \nfirstNum: ${firstNum}\nsecondNum: ${secondNum}`);
-     });
-     button.addEventListener("mousedown", () => {
-          button.style.filter = `brightness(${150}%)`;
-     });
-     button.addEventListener("mouseup", () => {
-          button.style.filter = `brightness(${100}%)`;
-     });
-});
-
-
-
-let equalButton = buttonsDiv.querySelector(".equals");
-equalButton.addEventListener("click", () => {
-     equal(firstNum, secondNum, operation);
-     equalButton.addEventListener("mousedown", () => {
-          equalButton.style.filter = `brightness(${125}%)`;
-     });
-     equalButton.addEventListener("mouseup", () => {
-          equalButton.style.filter = `brightness(${100}%)`;
-     });
-     operationButtons.forEach((button) => {
-          button.style.filter = `brightness(${100}%)`;
-     });
-     if (firstNum && secondNum && operation) {
-          firstNum = equal(firstNum, secondNum, operation);
-          console.log("This is the new firstNum", firstNum)
-     }
-});
+//           console.log(`operation: ${operation} \nfirstNum: ${firstNum}\nsecondNum: ${secondNum}`);
+//      });
+//      button.addEventListener("mousedown", () => {
+//           button.style.filter = `brightness(${150}%)`;
+//      });
+//      button.addEventListener("mouseup", () => {
+//           button.style.filter = `brightness(${100}%)`;
+//      });
+// });
 
 
 
-//Add event listener for decimal that when it is pressed, it gets appended to the the output text
+// let result;
 
-//OPERATIONS
+// let equalButton = buttonsDiv.querySelector(".equals");
+// equalButton.addEventListener("click", () => {
+//      if (firstNum && secondNum && operation) {
+//           firstNum = equal(firstNum, secondNum, operation);
+//           console.log("This is the new firstNum", firstNum);
+//           outputText.textContent = firstNum; // Update the output with the new firstNum
+//           // Reset secondNum and operation for the next calculation
+//           secondNum = null;
+//           operation = null;
+//      }
+//      operationButtons.forEach((button) => {
+//           button.style.filter = `brightness(${100}%)`;
+//      });
+// });
+
+
+// function appendToDisplay(buttonValue) {
+//      outputText.textContent += buttonValue;
+// }
 
 
 
-function clear() {
-     firstNum = null;
-     secondNum = null;
-     operation = null;
-     outputText.textContent = 0;
-}
+// //Add event listener for decimal that when it is pressed, it gets appended to the the output text
 
-function signChange(num) {
-     return num * -1;
-}
 
-function back() {
+// //OPERATIONS
 
-}
-//Create a variable for firstNum, secondNum, and operation
-// takes 2 parameter, firstNum secondNum(maybe an event)
-// first a number is clicked and added to variable firstNum that holds 2st number
-//  Then the function operation is called on the click of that operation symbol. Inside the function it creates a variable maybe addTemp that on a click of another number, it gets assigned to that variable, then the two numbers get added and then the answer is given the value of firstNum and returned to the output text awaiting the next operation 
-function add(firstNum, secondNum) {
-     // console.log("Addition Answer:",firstNum + secondNum);
-     return outputText.textContent = firstNum + secondNum;
-}
 
-function subtract(firstNum, secondNum) {
-     return outputText.textContent = firstNum - secondNum;
-}
 
-function divide(firstNum, secondNum) {
-     return outputText.textContent = firstNum / secondNum;
-}
+// function clear() {
+//      firstNum = null;
+//      secondNum = null;
+//      operation = null;
+//      outputText.textContent = 0;
+// }
 
-function multiply(firstNum, secondNum) {
-     return outputText.textContent = firstNum * secondNum;
-}
+// function signChange(num) {
+//      return num * -1;
+// }
 
-function equal(firstNum, secondNum, operation) {
-     switch(operation) {
-          case "+": 
-          return add(firstNum, secondNum);
-          break;
+// function back() {
 
-          case "-": 
-          return subtract(firstNum, secondNum);
-          break;
+// }
+// //Create a variable for firstNum, secondNum, and operation
+// // takes 2 parameter, firstNum secondNum(maybe an event)
+// // first a number is clicked and added to variable firstNum that holds 2st number
+// //  Then the function operation is called on the click of that operation symbol. Inside the function it creates a variable maybe addTemp that on a click of another number, it gets assigned to that variable, then the two numbers get added and then the answer is given the value of firstNum and returned to the output text awaiting the next operation 
+// function add(firstNum, secondNum) {
+//      // console.log("Addition Answer:",firstNum + secondNum);
+//      return outputText.textContent = firstNum + secondNum;
+// }
 
-          case "/": 
-          return divide(firstNum, secondNum);
-          break;
+// function subtract(firstNum, secondNum) {
+//      return outputText.textContent = firstNum - secondNum;
+// }
 
-          case "*": 
-          return multiply(firstNum, secondNum);
-     }
+// function divide(firstNum, secondNum) {
+//      return outputText.textContent = firstNum / secondNum;
+// }
 
-     // return operation;
+// function multiply(firstNum, secondNum) {
+//      return outputText.textContent = firstNum * secondNum;
+// }
 
-}
+// function equal(firstNum, secondNum, operation) {
+//      switch(operation) {
+//           case "+": 
+//           return add(firstNum, secondNum);
+//           break;
+
+//           case "-": 
+//           return subtract(firstNum, secondNum);
+//           break;
+
+//           case "/": 
+//           return divide(firstNum, secondNum);
+//           break;
+
+//           case "*": 
+//           return multiply(firstNum, secondNum);
+//      }
+
+//      // return operation;
+
+// }
