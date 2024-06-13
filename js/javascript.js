e58c2f0df43d340796e8773bb759ae5750415146
Andy Garcia <barcag07@icloud.com>
2024-06-13 17:35:51 -0500

    Fix starting over if no operator is selected
    If an answer is gotten, and the user does not click an operator to
    make a new equation, the first number clicked will turn into a new
    firstNum and the equation will start over.

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,23 @@
 const calculator = document.querySelector(".calculator");
-//If we want to get the style property from a css file, we would have to use getComputedStyle of whatever it is we want the style of. Then you can get your property
-//getComputedStyle will return a string because of the unit Ex: "100px"
 let calcCSS = getComputedStyle(calculator);
 let calcWidth = parseFloat(calcCSS.width);
 
-
 const buttonsDiv = document.querySelector(".buttons");
-
 
 const buttonItems = buttonsDiv.querySelectorAll("button");
 let buttonMargin = 5;
 let buttonPadding = 0;
 let totalButtonHW = calcWidth / 4;
 let buttonHW = totalButtonHW - (buttonMargin * 2) - (buttonPadding * 2);
-// console.log(buttonHW);
-buttonItems.forEach((button) => {
-     button.style.height = buttonHW + "px";
-     button.style.width = buttonHW + "px";
-     button.style.borderRadius = 100 + "%";
-     button.style.border = "none";
-     button.style.margin = buttonMargin + "px";
-     button.style.padding = buttonPadding + "px";
-});
 
+buttonItems.forEach((button) => {
+    button.style.height = buttonHW + "px";
+    button.style.width = buttonHW + "px";
+    button.style.borderRadius = 100 + "%";
+    button.style.border = "none";
+    button.style.margin = buttonMargin + "px";
+    button.style.padding = buttonPadding + "px";
+});
 
 let zeroButton = buttonsDiv.querySelector(".zero");
 let zeroButtonWidth = buttonHW * 2 + buttonMargin * 2 + buttonPadding * 2;
@@ -31,157 +26,154 @@ zeroButton.style.borderRadius = 100 + "px";
 zeroButton.style.margin = buttonMargin;
 zeroButton.style.padding = buttonPadding;
 
-
-
-
 let calcButtons = buttonsDiv.querySelectorAll("button");
 calcButtons.forEach((button) => {
-     button.addEventListener("mousedown", () => {
-          button.style.filter = `brightness(${150}%)`;
-     });
+    button.addEventListener("mousedown", () => {
+        button.style.filter = `brightness(${150}%)`;
+    });
 
-     button.addEventListener("mouseup", () => {
-          button.style.filter = `brightness(${100}%)`;
-     })
-
+    button.addEventListener("mouseup", () => {
+        button.style.filter = `brightness(${100}%)`;
+    })
 });
 
 let tempNum;
 
 let numberButtons = buttonsDiv.querySelectorAll(".number");
 numberButtons.forEach((button) => {
-     button.addEventListener("click", () => {
-          buttonValue = button.getAttribute("data-value");
+    button.addEventListener("click", () => {
+        let buttonValue = button.getAttribute("data-value");
 
-          if (firstNum && !operationButtonTF){
-               outputText.value = "";
-               firstNum = outputText.value;
+        if (resultDisplayed) {
+            // Reset the display when a new number is clicked after a result is displayed
+            outputText.value = "";
+            resultDisplayed = false;
+            firstNum = undefined;
+            secondNum = undefined;
+            operator = null;
+        }
 
-          }
+        if (operationButtonTF) {
+            // Reset outputText for the second number
+            outputText.value = "";
+            operationButtonTF = false;
+        }
 
-          if (buttonValue === ".") {;
-               if (!getCurrentNum().includes(".")) {
-                    appendDecimal();
-               }
-          }
-          else {
-               buttonValue = Number(buttonValue);
-               // console.log(typeof buttonValue);
-               tempNum = appendToDisplay(buttonValue);
+        if (buttonValue === ".") {
+            // Handle decimal point
+            if (!outputText.value.includes(".")) {
+                appendToDisplay(buttonValue);
+            }
+        } else {
+            // Append digit to the current number
+            appendToDisplay(buttonValue);
+        }
 
-               if (firstNum && operator) {
+        // Update firstNum or secondNum based on the presence of an operator
+        if (!operator) {
+            firstNum = Number(outputText.value);
+        } else {
+            secondNum = Number(outputText.value);
+        }
 
-                    secondNum = Number(outputText.value);
-               }
-               // console.log(showNum);
-          }  
-          
-     });
+        console.log(`firstNum: ${firstNum}, operator: ${operator}, secondNum: ${secondNum}`);
+    });
 });
 
+function appendToDisplay(input) {
+    outputText.value += input;
+}
 
 let firstNum;
 let secondNum;
 let operator;
 let operationButtonTF = false;
+let resultDisplayed = false;
 
 let operationButton = buttonsDiv.querySelectorAll(".operator");
 operationButton.forEach((button) => {
-     button.addEventListener("click", () => {
-          operator = button.getAttribute("data-value");
-          operationButtonTF = true;
-          if (!firstNum) {
-               firstNum = Number(outputText.value);
-               outputText.value = "";
-          }
-          else {
-               outputText.value = "";
-          }
-          
-
-               // console.log(showNum);
-     });
+    button.addEventListener("click", () => {
+        operator = button.getAttribute("data-value");
+        operationButtonTF = true;
+        if (!firstNum) {
+            firstNum = Number(outputText.value);
+            outputText.value = "";
+        } else {
+            outputText.value = "";
+        }
+    });
 });
 
-
 function getCurrentNum() {
-     let currentNum = outputText.value;
-     return currentNum
+    let currentNum = outputText.value;
+    return currentNum;
 }
-
-
 
 let outputTextContainer = calculator.querySelector(".output-container");
 let outputText = outputTextContainer.querySelector(".output-text");
-// outputText.style.color = "red";
-
 
 let equalButton = buttonsDiv.querySelector(".equals");
 equalButton.addEventListener("click", () => {
-     outputText.value = operate(firstNum, secondNum, operator);
-     result = Number(outputText.value);
-     firstNum = result;
-     operationButtonTF = false;
-     secondNum = undefined;
-})
+    outputText.value = operate(firstNum, secondNum, operator);
+    result = Number(outputText.value);
+    firstNum = result;
+    operationButtonTF = false;
+    secondNum = undefined;
+    resultDisplayed = true;  // Set the flag indicating the result is displayed
+});
 
 let clearButton = buttonsDiv.querySelector(".clear");
 clearButton.addEventListener("click", () => {
-     // clearButton.style.backgroundColor = "red";
-     firstNum = "";
-     secondNum = "";
-     operator = null;
-     outputText.value = "";
+    firstNum = "";
+    secondNum = "";
+    operator = null;
+    outputText.value = "";
+    resultDisplayed = false;
 });
 
-
-
+let signChangeButton = buttonsDiv.querySelector(".positive-negative");
+signChangeButton.addEventListener("click", () => {
+    // signChangeButton.style.backgroundColor = "blue";
+});
 
 function appendDecimal() {
-     outputText.value += ".";
-}
-
-
-function appendToDisplay(input) {
-     outputText.value += input;
+    outputText.value += ".";
 }
 
 let decimalAnswer;
 
 function add(firstNum, secondNum) {
-     return firstNum + secondNum;
-}
-function subtract(firstNum, secondNum) {
-     return firstNum - secondNum;
-}
-function multiply(firstNum, secondNum) {
-     decimalAnswer = firstNum * secondNum;
-     return Math.round(decimalAnswer * 100) / 100;
-}
-function divide(firstNum, secondNum) {
-     if (firstNum === 0 || secondNum === 0) {
-          return 0
-     }
-     else {
-          decimalAnswer = firstNum / secondNum;
-          return Math.round(decimalAnswer * 100) / 100;
-     }
+    return firstNum + secondNum;
 }
 
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+    decimalAnswer = firstNum * secondNum;
+    return Math.round(decimalAnswer * 100) / 100;
+}
+
+function divide(firstNum, secondNum) {
+    if (firstNum === 0 || secondNum === 0) {
+        return 0;
+    } else {
+        decimalAnswer = firstNum / secondNum;
+        return Math.round(decimalAnswer * 100) / 100;
+    }
+}
 
 function operate(firstNum, secondNum, operator) {
-     //switch statement
-
-     switch(operator) {
-          case "+": 
-               return add(firstNum, secondNum);
-          case "-":
-               return subtract(firstNum, secondNum);
-
-          case "*":
-               return multiply(firstNum, secondNum);
-
-          case "/":
-               return divide(firstNum, secondNum);
-     }
+    //switch statement
+    switch(operator) {
+        case "+": 
+            return add(firstNum, secondNum);
+        case "-":
+            return subtract(firstNum, secondNum);
+        case "*":
+            return multiply(firstNum, secondNum);
+        case "/":
+            return divide(firstNum, secondNum);
+    }
 }
